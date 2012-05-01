077bcea8dfd5079fba6784bbafa1e786bb3b964b
Ziliang Guo <ziliang@cs.wisc.edu>
2012-05-01 16:08:03 -0500

    Add parallel class.

---
+++ woodcock/Parallel.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package woodcock;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ *
+ * @author Z98
+ */
+public class Parallel {
+    static final int CPUs = Runtime.getRuntime().availableProcessors();
+    
+    public interface Each {
+        void run(int i);
+    }
+    
+    public static void withIndex(int start, int stop, final Each body) {
+        int chunksize = (stop - start + CPUs - 1) / CPUs;
+        int loops = (stop - start + chunksize - 1) / chunksize;
+        ExecutorService executor = Executors.newFixedThreadPool(CPUs);
+        final CountDownLatch latch = new CountDownLatch(loops);
+        for (int i = start; i < stop;) {
+            final int lo = i;
+            i += chunksize;
+            final int hi = (i < stop) ? i : stop;
+            executor.submit(new Runnable() {
+
+                public void run() {
+                    for (int i = lo; i < hi; i++) {
+                        body.run(i);
+                    }
+                    latch.countDown();
+                }
+            });
+        }
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+        }
+        executor.shutdown();
+    }
+}
