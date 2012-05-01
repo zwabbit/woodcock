e1e7e89fbeb5ec9b48984e16f831fccbb3b8e7cd
Ziliang Guo <ziliang@cs.wisc.edu>
2012-05-01 10:34:38 -0500

    Add agent classes.

---
+++ woodcock/LumberCompany.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package woodcock;
+
+/**
+ *
+ * @author Z98
+ */
+public class LumberCompany {
+    /*
+     * This class goes through forest patches
+     * in order to order them based on how desirable
+     * or profitable cutting a patch would be.
+     */
+}
---
+++ woodcock/WCConservation.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package woodcock;
+
+/**
+ *
+ * @author Z98
+ */
+public class WCConservation {
+    /*
+     * This class handles searching for candidate
+     * habitats where woodcock can live and finding
+     * candidates for cutting to create these habitats.
+     */
+    
+}
