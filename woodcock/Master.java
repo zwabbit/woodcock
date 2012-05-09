404a53fde7ced7f37859ebbfe2de133f4935d579
Ziliang Guo <ziliang@cs.wisc.edu>
2012-05-08 20:16:29 -0500

    Code cleanup.

--- woodcock/Master.java
+++ woodcock/Master.java
@@ -13,8 +13,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JFrame;
 
 /**
